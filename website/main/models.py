976a76d23710d6650ac06d203e2bac789bc03e71
2nytwin <2nytwinconnect@gmail.com>
2022-12-10 03:21:24 +0300

    meta

--- website/main/models.py
+++ website/main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField('FCS', max_length=250)
-    task = models.TextField('Description')
+    title = models.CharField('Название задачи', max_length=50)
+    task = models.TextField('Описание')
 
     def __str__(self):
         return self.title
