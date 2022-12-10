ba4b3745afc3003dad0f36641b7e3a809dc842b1
2nytwin <2nytwinconnect@gmail.com>
2022-12-10 03:10:05 +0300

    meta

--- website/main/models.py
+++ website/main/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField('Name', max_length=250)
+    title = models.CharField('FCS', max_length=250)
     task = models.TextField('Description')
-    number = models.CharField('№', max_length=9)
-    FCS = models.TextField('FCs')
 
+    def __str__(self):
+        return self.title
 
-def __str__(self):
-    return self.title
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
--- website/main/views.py
+++ website/main/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 
 def index(request):
-    return render(request, 'main/index.html')
-
-
-def findme(request):
-    return render(request, 'findme.html')
+    task = Task.objects.all()
+    return render(request, 'main/index.html',{'title':'Главная страница', 'tasks':'title'})
 
 
 def about(request):
     return render(request, 'main/about.html')
 
 
-def test(request):
-    return render(request, 'main/test.html')
