bb9c3c00f4f27686525f1943f827cf255b6dc3f2
2nytwin <2nytwinconnect@gmail.com>
2022-12-29 03:51:43 +0300

    create

--- website/main/views.py
+++ website/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
@@ -13,4 +14,8 @@ def about(request):
 
 
 def create(request):
+    form = TaskForm()
+    context = {
+'form':form
+    }
     return render(request, 'main/create.html')
